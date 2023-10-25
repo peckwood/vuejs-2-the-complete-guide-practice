a41ad0eeaeb2835ad4ff3188eac441dfb18e07b3
raiden <peckwood@users.noreply.github.com>
2023-10-25 23:49:41 +0800

    section 4 complete

--- section-04-course-project-monster-slayer-game/prj-monster-01-starting-setup/app.js
+++ section-04-course-project-monster-slayer-game/prj-monster-01-starting-setup/app.js
@@ -10,14 +10,60 @@ Vue.createApp({
             monsterHealth: 100,
             specialAttackCD: 4,
             currentSpecialAttackCD: 0,
-            healCD: 4,
-            currentHealCD: 0,
+            winner: undefined,
+            battleLog: []
+        }
+    },
+    methods: {
+        startNewGame() {
+            this.winner = null;
+            this.monsterHealth = this.monsterMaxHealth;
+            this.playerHealth = this.playerMaxHealth;
+            this.currentSpecialAttackCD = 0;
+            this.battleLog = [];
+        },
+        surrender() {
+            this.winner = 'monster';
+        },
+        attackMonster(){
+            const attackValue = getRandomValue(5, 12);
+            this.monsterHealth -= attackValue;
+            this.addAttackLog('player', 'normal attack', 'monster', attackValue, this.monsterHealth);
+            this.attackPlayer();
+            this.endRound();
+        },
+        attackPlayer(){
+            const attackValue = getRandomValue(8, 15);
+            this.playerHealth -= attackValue;
+            this.addAttackLog('monster', 'normal attack', 'player', attackValue, this.playerHealth);
+        },
+        specialAttackMonster(){
+            const attackValue = getRandomValue(10, 25);
+            this.monsterHealth -= attackValue;
+            this.currentSpecialAttackCD += this.specialAttackCD;
+            this.addAttackLog('player', 'special attack', 'monster', attackValue, this.monsterHealth);
+            this.attackPlayer();
+            this.endRound();
+        },
+        addAttackLog(attacker, attackType, victim, damage, remainingHealth){
+            const log = attacker + ' ' + attackType + ' ' + victim + ' for ' + damage + ' damage, ' + victim + ' now has ' + remainingHealth + ' health';
+            this.battleLog.unshift(log);
+        },
+        endRound(){
+            this.currentSpecialAttackCD = this.currentSpecialAttackCD - 1 < 0 ? 0 : this.currentSpecialAttackCD - 1;
+            this.battleLog.unshift('---------------------------------------------------------------------------');
+        },
+        healPlayer(){
+            const healValue = getRandomValue(8, 20);
+            const resultHealth = this.playerHealth + healValue;
+            this.playerHealth = resultHealth > this.playerMaxHealth ? this.playerMaxHealth : resultHealth;
+            const log = 'player heals himself for ' + healValue + ', player now has ' + this.playerHealth + ' health';
+            this.battleLog.unshift(log);
+            this.attackPlayer();
+            this.endRound();
         }
     },
     computed: {
-        canHeal() {
-            return this.currentHealCD <= 0;
-        },
         canSpecialAttack() {
           return this.currentSpecialAttackCD <= 0;
         },
@@ -40,45 +86,24 @@ Vue.createApp({
             }
         },
     },
-    methods: {
-        attackMonster(){
-            const attackValue = getRandomValue(5, 12);
-            this.monsterHealth -= attackValue;
-            this.attackPlayer();
-            this.endRound();
-        },
-        attackPlayer(){
-            const attackValue = getRandomValue(8, 15);
-            this.playerHealth -= attackValue;
-        },
-        specialAttackMonster(){
-            const attackValue = getRandomValue(10, 25);
-            this.monsterHealth -= attackValue;
-            this.currentSpecialAttackCD += this.specialAttackCD;
-            this.attackPlayer();
-            this.endRound();
-        },
-        endRound(){
-            this.currentSpecialAttackCD = this.currentSpecialAttackCD - 1 < 0 ? 0 : this.currentSpecialAttackCD - 1;
-            this.currentHealCD = this.currentHealCD - 1 < 0 ? 0 : this.currentHealCD - 1;
-        },
-        healPlayer(){
-            const healValue = getRandomValue(8, 20);
-            const resultHealth = this.playerHealth + healValue;
-            this.playerHealth = resultHealth > this.playerMaxHealth ? this.playerMaxHealth : resultHealth;
-            this.currentHealCD = this.healCD;
-            this.attackPlayer();
-            this.endRound();
-        }
-    },
     watch: {
         playerHealth(value){
-            if(value < 0){
+            if(value <= 0 && this.monsterHealth <= 0){
+                this.winner = 'draw';
+            }else if(value <= 0){
+                this.winner = 'monster';
+            }
+            if(value<0){
                 this.playerHealth = 0;
             }
         },
         monsterHealth(value){
-            if(value < 0){
+            if(value <= 0 && this.playerHealth <= 0){
+                this.winner = 'draw';
+            }else if(value <= 0){
+                this.winner = 'player';
+            }
+            if(value<0){
                 this.monsterHealth = 0;
             }
         }
