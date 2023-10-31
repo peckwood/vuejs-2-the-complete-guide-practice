f2f1bba02ebfbfcab44900cbc71a6118a2c3afb0
raiden <peckwood@users.noreply.github.com>
2023-10-31 23:45:19 +0800

    section 7 complete

---
+++ section-07-move-to-vue-cli/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,30 @@
+<template>
+  <h2>{{friend.name}}</h2>
+  <button @click="toggleDetails">{{detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+  <ul v-if="detailsAreVisible">
+    <li><strong>Phone:</strong> {{friend.phone}}</li>
+    <li><strong>Email:</strong> {{friend.email}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    friend: {
+      required: false,
+      type: Object
+    }
+  },
+  data() {
+    return {
+      detailsAreVisible: true,
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+
+</script>
