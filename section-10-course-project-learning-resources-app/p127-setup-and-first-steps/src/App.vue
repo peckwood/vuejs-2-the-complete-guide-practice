91cef3d0098345a476a13ceb0a05b12607c7a6ad
raiden <peckwood@users.noreply.github.com>
2023-11-18 22:37:29 +0800

    129. Styling and More Components - complete

--- section-10-course-project-learning-resources-app/p127-setup-and-first-steps/src/App.vue
+++ section-10-course-project-learning-resources-app/p127-setup-and-first-steps/src/App.vue
@@ -1,47 +1,36 @@
 <template>
-  <ul>
-    <learning-resource
-      v-for='resource in storedResources'
-      :key='resource.id'
-      :title='resource.title'
-      :description='resource.description'
-      :link='resource.link'
-    ></learning-resource>
-  </ul>
+  <stored-resources></stored-resources>
 </template>
 
 <script>
-import LearningResource from '@/components/learning-resources/LearningResource.vue';
+import StoredResources from '@/components/learning-resources/StoredResources.vue';
 
 export default {
   components: {
-    LearningResource
+    StoredResources,
   },
   data() {
     return {
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation',
-          link: 'http://vuejs.org'
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn to google',
-          link: 'https://baidu.com'
-        }
-      ]
+
     }
   }
 };
 </script>
 
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
 
+* {
+  box-sizing: border-box;
+}
 
+html {
+  font-family: 'Roboto', sans-serif;
 
+}
 
-<style scoped>
+body {
+  margin: 0;
 
+}
 </style>
---
+++ section-10-course-project-learning-resources-app/p127-setup-and-first-steps/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,48 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for='resource in storedResources'
+      :key='resource.id'
+      :title='resource.title'
+      :description='resource.description'
+      :link='resource.link'
+    ></learning-resource>
+  </ul>
+</template>
+<script>
+
+import LearningResource from '@/components/learning-resources/LearningResource.vue';
+
+export default {
+  components: { LearningResource },
+  data(){
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'http://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google',
+          link: 'https://baidu.com'
+        }
+      ]
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
