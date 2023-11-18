0b2312575d6705f9c9364579aa3cc358c9dcb2e2
raiden <peckwood@users.noreply.github.com>
2023-11-18 17:48:52 +0800

    127 complete

---
+++ section-10-course-project-learning-resources-app/p127-setup-and-first-steps/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul></ul>
+</template>
+
+<script setup>
+export default {
+  storedResources: [
+    {
+      id: 'official-guide',
+      title: 'Official Guide',
+      description: 'The official Vue.js documentation',
+      link: 'http://vuejs.org'
+    },
+    {
+      id: 'google',
+      title: 'Google',
+      description: 'Learn to google',
+      link: 'https://baidu.com'
+    }
+  ]
+};
+</script>
+
+
+
+<style scoped>
+
+</style>
