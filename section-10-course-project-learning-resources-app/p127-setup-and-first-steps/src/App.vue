38128aa378830f14605b988418714c0a7fb4faeb
raiden <peckwood@users.noreply.github.com>
2023-11-18 22:20:51 +0800

    128- First component and props - complete

--- section-10-course-project-learning-resources-app/p127-setup-and-first-steps/src/App.vue
+++ section-10-course-project-learning-resources-app/p127-setup-and-first-steps/src/App.vue
@@ -1,25 +1,44 @@
 <template>
-  <ul></ul>
+  <ul>
+    <learning-resource
+      v-for='resource in storedResources'
+      :key='resource.id'
+      :title='resource.title'
+      :description='resource.description'
+      :link='resource.link'
+    ></learning-resource>
+  </ul>
 </template>
 
-<script setup>
+<script>
+import LearningResource from '@/components/learning-resources/LearningResource.vue';
+
 export default {
-  storedResources: [
-    {
-      id: 'official-guide',
-      title: 'Official Guide',
-      description: 'The official Vue.js documentation',
-      link: 'http://vuejs.org'
-    },
-    {
-      id: 'google',
-      title: 'Google',
-      description: 'Learn to google',
-      link: 'https://baidu.com'
+  components: {
+    LearningResource
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'http://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google',
+          link: 'https://baidu.com'
+        }
+      ]
     }
-  ]
+  }
 };
 </script>
+
+
 
 
 
