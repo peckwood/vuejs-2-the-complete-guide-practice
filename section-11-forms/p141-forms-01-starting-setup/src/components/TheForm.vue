c7df699aadcdcfefd005c6a7c88606fdf7a6f6d4
raiden <peckwood@users.noreply.github.com>
2023-12-19 23:48:28 +0800

    p141 - v-model & inputs - complete

--- section-11-forms/p141-forms-01-starting-setup/src/components/TheForm.vue
+++ section-11-forms/p141-forms-01-starting-setup/src/components/TheForm.vue
@@ -1,8 +1,8 @@
 <template>
-  <form>
+  <form @submit.prevent='submitForm'>
     <div class="form-control">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" />
+      <input id="user-name" name="user-name" type="text" v-model='userName'/>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -52,6 +52,23 @@
   </form>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      userName: '',
+
+    }
+  },
+  methods: {
+    submitForm() {
+      console.log('username', this.userName);
+      this.userName = '';
+    }
+  }
+}
+
+</script>
 <style scoped>
 form {
   margin: 2rem auto;
