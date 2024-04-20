d067d134a3ab55b4e53c8ebda81fdd3065de6ad5
raiden <peckwood@users.noreply.github.com>
2024-04-20 16:19:16 +0800

    206. Getting Started with Animated Lists - complete

---
+++ section-14-animation/p194-animations-01-starting-setup/src/components/UsersList.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul>
+    <li v-for='user in users' :key='user' @click='removeUser(user)'>
+      {{user}}
+    </li>
+  </ul>
+  <div>
+    <input type='text' ref='newUserName'/>
+    <button @click='addUser'>Add User</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return {
+      users: ['Max', 'Manu', 'Julie', 'Angela', 'Michael']
+    }
+  },
+  methods: {
+    addUser(){
+      const user = this.$refs.newUserName.value;
+      this.users.unshift(user);
+    },
+    removeUser(user){
+      this.users = this.users.filter(existingUser => existingUser !== user);
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 1em 0;
+  padding: 0;
+  text-align: center;
+}
+li {
+  padding: 1em;
+  border: 1px solid #ccc;
+}
+</style>
