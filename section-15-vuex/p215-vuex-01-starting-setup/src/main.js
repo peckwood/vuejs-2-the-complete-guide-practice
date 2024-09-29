f42640cba8c82630770a15de5499ac5214652037
raiden <peckwood@users.noreply.github.com>
2024-09-29 23:58:54 +0800

    219 Introducing Getters - A Better Way Of Getting Data - complete

--- section-15-vuex/p215-vuex-01-starting-setup/src/main.js
+++ section-15-vuex/p215-vuex-01-starting-setup/src/main.js
@@ -13,6 +13,21 @@ const store = createStore({
     increase(state, payload) {
       state.counter = state.counter + payload;
     }
+  },
+  getters: {
+    finalCounter(state){
+      return state.counter * 2;
+    },
+    normalizedCounter(_, getters){
+      const finalCounter = getters.finalCounter;
+      if(finalCounter < 0){
+        return 0;
+      }else if(finalCounter> 100){
+        return 100;
+      }else{
+        return finalCounter;
+      }
+    }
   }
 });
 
