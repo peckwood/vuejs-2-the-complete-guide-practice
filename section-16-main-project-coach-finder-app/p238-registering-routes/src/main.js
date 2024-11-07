8892e22b59f8293dff91725ec8ceb5c7912d0276
raiden <peckwood@users.noreply.github.com>
2024-11-07 23:53:14 +0800

    239. Adding Route Page Components - complete

--- section-16-main-project-coach-finder-app/p238-registering-routes/src/main.js
+++ section-16-main-project-coach-finder-app/p238-registering-routes/src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue';
 import router from "@/router";
+import app from "@/App.vue";
+
+
 
 const app = createApp({}).mount('#app');
 
 app.use(router);
+
+app.mount('#app')
--- section-16-main-project-coach-finder-app/p238-registering-routes/src/router.js
+++ section-16-main-project-coach-finder-app/p238-registering-routes/src/router.js
@@ -1,18 +1,29 @@
 import {createRouter, createWebHistory} from "vue-router";
 
+import CoachDetail from "@/pages/coaches/CoachDetail.vue";
+import CoachesList from "@/pages/coaches/CoachesList.vue";
+import CoachRegistration from "@/pages/coaches/CoachRegistration.vue";
+import ContactCoach from "@/pages/requests/ContactCoach.vue";
+import RequestsReceived from "@/pages/requests/RequestsReceived.vue";
+import notFound from "@/pages/NotFound.vue";
+import coachDetail from "@/pages/coaches/CoachDetail.vue";
+import contactCoach from "@/pages/requests/ContactCoach.vue";
+import coachRegistration from "@/pages/coaches/CoachRegistration.vue";
+import requestsReceived from "@/pages/requests/RequestsReceived.vue";
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', redirect: '/coaches'},
-        {path: '/coaches', component: null},
+        {path: '/coaches', component: CoachesList},
         {
-            path: '/coaches/:id', component: null, children: [
-                {path: 'contact', component: null},
+            path: '/coaches/:id', component: coachDetail, children: [
+                {path: 'contact', component: contactCoach},// /coaches/c1/contact
             ]
         },
-        {path: '/register', component: null},
-        {path: '/requests', component: null},
-        {path: '/:notFound(.*)', component: null},
+        {path: '/register', component: coachRegistration},
+        {path: '/requests', component: requestsReceived},
+        {path: '/:notFound(.*)', component: notFound},
 
     ]
 })
